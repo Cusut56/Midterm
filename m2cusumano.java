209699dad5c8954d90400bb1cba3b23d54370327
Cusut56 <tarashere2005@yahoo.com>
2015-10-29 20:23:13 -0400

    Update and rename m1cusumano.java to m2cusumano.java

--- m2cusumano.java
+++ m2cusumano.java
@@ -1,3 +1,4 @@
+
 ///// Midterm code for 59CST112
 float left,right,top,bottom;
 float middle;
@@ -6,18 +7,23 @@ float cueX,cueY,cueDX,cueDY;
 float orangeX,orangeY,orangeDX,orangeDY;
 float purpleX,purpleY,purpleDX,purpleDY;
 float greenX,greenY,greenDX,greenDY;
+int x=50;
+int y=50;
+int w =100;
+int h =75;
 
 
 
 void setup(){
 size(680,480);
 left=65;
-right= width-25;
+right= width-75;
 top= 100;
 bottom=height-50;
 middle= left+(right-left)/2;
 reset();
 }
+
 void reset(){
   cueX=left +(right-left)/2;
   cueY=top+(bottom-top)/2;
@@ -29,34 +35,54 @@ void reset(){
  greenX=random(middle,right); greenY=random(top,bottom);
   
   //random speed
- orangeDX= random(1,3); orangeDY=random(1,3);
+ orangeDX= random(2,3); orangeDY=random(1,3);
   purpleDX=random(1,3); purpleDY=random(1,3);
   greenDX=random(1,3); greenDY=random(1,3);
 }
-
+{
+left=75;
+right = width-50;
+top=100;
+bottom= height-25;
+}
 void draw(){
   background(255,255,200);
    rectMode(CORNERS);
     fill(50,205,50);
-   rect(left,top,right,bottom);
- 
+    rect(left,top,right,bottom);
+   
+
+
+ fill(175);
+ rect(x,y,w,h);
+
   bounce();
     show();
-    collision();
+    
 
 }
 
-void bounce(){
-  orangeX=  orangeX + orangeDX;
-  if((orangeX>width) ||(orangeX<0))
-    ;
-    purpleX= purpleX+purpleDX;
-  if((purpleX>width)||(purpleX<0))
-    ;
-    greenX= greenX +greenDX;
-  if((greenX>width)||(greenX<0))
-    ;
+  void bounce(){
+ 
+    purpleX=purpleDX; if (purpleX<left|| purpleX>right) purpleDX *= -1;
+    orangeX=orangeDX; if (orangeX<left||orangeX>right) orangeDX *= -1;
+    greenX=greenDX;   if (greenX<left ||greenY>right) greenDX *= -1;
 }
+
+
+
+
+//collision(){
+float tmp;{
+//Swap velocities!
+if (dist (purpleX, purpleY, orangeX, orangeY) <50){
+  tmp=orangeDX;orangeDX=purpleDY; purpleDX=tmp;
+  tmp=orangeDY; orangeDY=purpleDY; purpleDX=tmp;
+ 
+}
+
+}
+
 
 //show balls
 void show(){
@@ -67,11 +93,4 @@ void show(){
  fill(90,80,250); ellipse(purpleX,purpleY,30,30);
   
  fill(50,200,0); ellipse(greenX,greenY,30,30);
-  
-
-
-}
-//collision
-void collision(){
-
 }
